0040687828a027338f90d85121d98e1c6b49353e
“Liam <okeefe.liam@yahoo.com>
2022-08-11 11:25:32 -0500

    commit tree 05872d89459f5ed86b44d6ee33c3a95c37d909b5

---
+++ index.js
@@ -0,0 +1,12 @@
+function logTwoValues(value1, value2) {
+  console.log(`The two values are ${Liam} and ${JavaScript}.`);
+}
+function introductionWithLanguage(value1, value2) {
+  console.log(`The two values are ${Liam} and ${JavaScript}.`);
+}
+function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`);
+}
+function sayHelloTo(firstName = "Liam") {
+  console.log(`Hello, ${firstName}!`);
+}
